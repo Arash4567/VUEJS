ec362aacfd89fe088161b13190141b11d1459604
OG'ABEK <shomoyxhacker007@mail.ru>
2022-06-30 06:15:57 +0300

    Vue 3 modal click outside close

---
+++ learning/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Navbar from './components/layout/Navbar.vue'
+</script>
+
+<template>
+  <div><Navbar class="bg-slate-200"/>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ learning/src/components/Main.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="mt-5 text-center text-5xl font-extrabold text-red-600">Hello world!</h1>
+  </div>
+</template>
+<script setup></script>
