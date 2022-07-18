1b4e9b552c25a51647bf524de22da6fb62642c31
OG'ABEK <shomoyxhacker007@mail.ru>
2022-07-18 20:43:28 +0300

    some changes

--- learning/src/components/Main.vue
+++ learning/src/components/Main.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    <h1 class="mt-5 text-center text-5xl font-extrabold text-red-600">Hello world!</h1>
+    <h1 class="mt-5 text-center text-5xl font-extrabold text-red-600">Hello world! - {{ data }}</h1>
+    <div v-html="data"></div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { ref, reactive, watch } from 'vue'
+
+const data = ref("<b>sample data</b>")
+
+// Directivelar v- prefiksi bilan boshlanadigan atributlardir
+
+//---------- v-model modifiers -------------
+// v-model.number
+// v-model.lazy - enter bosganda statega yozadi
+// v-model.trim - whitespacelarni tozalaydi
+// v-model.trim.lazy - chaining ko`rinishi
+
+// --------- Methods & Events ---------------
+// --------- Lifecycle Hooks ---------------
+
+
+</script>
